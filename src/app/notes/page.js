8903a9e9112c011fce2bbfb194bdec611a024880
Ankit Mehta <ankitmmehta08@gmail.com>
2025-09-26 09:24:44 +0530

    basic setup and app test

---
+++ src/app/notes/page.js
@@ -0,0 +1,7 @@
+export default function NotesPage(){
+    return(
+<div>
+    this is a notes page
+</div>
+    )
+}
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function Login() {
+    return(
+        <>
+        <div className="login-container">
+            <h1 className="text-gradient">T Notes</h1>
+
+        </div>
+        </>
+    )
+}
